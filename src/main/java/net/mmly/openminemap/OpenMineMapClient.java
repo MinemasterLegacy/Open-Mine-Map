550de80e9a2069e0540606d37ff8f4fb5785b230
MinemasterLegacy <minemasterlegacy@gmail.com>
2026-01-09 12:39:14 -0700

    set mod version to 1.5.1

--- src/main/java/net/mmly/openminemap/OpenMineMapClient.java
+++ src/main/java/net/mmly/openminemap/OpenMineMapClient.java
@@ -18,7 +18,7 @@ import java.util.ArrayList;
 public class OpenMineMapClient implements ClientModInitializer { // client class
 
     public static ArrayList<String> debugMessages = new ArrayList<>();
-    public static final String MODVERSION = "1.5.0";
+    public static final String MODVERSION = "1.5.1";
 
     @Override
     public void onInitializeClient() { //method where other fabric api methods for registering and adding objects and behaviors will be called

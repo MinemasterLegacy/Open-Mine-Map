d89e44c3a367064fd64c4476041d2950a48b2483
MinemasterLegacy <minemasterlegacy@gmail.com>
2026-02-18 11:41:14 -0700

    version label of configscreen now uses a basic text draw

--- src/main/java/net/mmly/openminemap/config/ConfigScreen.java
+++ src/main/java/net/mmly/openminemap/config/ConfigScreen.java
@@ -11,6 +11,7 @@ import net.minecraft.client.util.Window;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.mmly.openminemap.OpenMineMapClient;
+import net.mmly.openminemap.draw.Justify;
 import net.mmly.openminemap.draw.UContext;
 import net.mmly.openminemap.enums.ButtonFunction;
 import net.mmly.openminemap.enums.ConfigOptions;
@@ -131,9 +132,9 @@ public class ConfigScreen extends Screen {
         this.addDrawableChild(exitButtonLayer);
         this.addDrawableChild(checkButtonLayer);
 
-        versionLabel = new TextWidget(0, windowScaledHeight - 16, windowScaledWidth - 5, 9, Text.of("OpenMineMap v" + OpenMineMapClient.MODVERSION), this.textRenderer);
-        versionLabel.alignRight();
-        this.addDrawableChild(versionLabel);
+        //versionLabel = new TextWidget(0, windowScaledHeight - 16, windowScaledWidth - 5, 9, Text.of("OpenMineMap v" + OpenMineMapClient.MODVERSION), this.textRenderer);
+        //versionLabel.alignRight();
+        //this.addDrawableChild(versionLabel);
 
         configHud = ButtonWidget.builder(Text.translatable("omm.config.option.configure-hud"), (btn) -> {
                 this.saveChanges();
@@ -215,5 +216,6 @@ public class ConfigScreen extends Screen {
         super.render(context, mouseX, mouseY, delta);
         //context.disableScissor();
         wikiLinkLayer.drawWidget(context, textRenderer);
+        UContext.drawJustifiedText(Text.of("OpenMineMap v" + OpenMineMapClient.MODVERSION), Justify.RIGHT, windowScaledWidth - 5, windowScaledHeight - 16, 0xFFFFFFFF);
     }
 }

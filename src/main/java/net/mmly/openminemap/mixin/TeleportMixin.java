5a53ad73ed4adbb928f13ef09035e95db2955553
MinemasterLegacy <minemasterlegacy@gmail.com>
2026-07-09 23:40:41 -0700

    change teleport intercept mixin to use @ModifyVariable

--- src/main/java/net/mmly/openminemap/mixin/TeleportMixin.java
+++ src/main/java/net/mmly/openminemap/mixin/TeleportMixin.java
@@ -2,19 +2,17 @@ package net.mmly.openminemap.mixin;
 
 import com.llamalad7.mixinextras.sugar.Local;
 import com.llamalad7.mixinextras.sugar.ref.LocalRef;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
-import net.minecraft.text.Text;
 import net.mmly.openminemap.enums.ConfigOptions;
 import net.mmly.openminemap.gui.RightClickMenu;
 import net.mmly.openminemap.projection.CoordinateValueError;
 import net.mmly.openminemap.projection.Projection;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
-import java.util.Arrays;
+import java.util.List;
 
 /*@Mixin(ClientPlayNetworkHandler.class)
 public class TeleportMixin {
@@ -28,36 +26,51 @@ public class TeleportMixin {
 @Mixin(ClientPlayNetworkHandler.class)
 public class TeleportMixin {
 
-    @Inject(method = "sendChatCommand", at = @At(value = "INVOKE", target = "Lnet/minecraft/command/argument/SignedArgumentList;of(Lcom/mojang/brigadier/ParseResults;)Lnet/minecraft/command/argument/SignedArgumentList;", shift = At.Shift.BEFORE))
-    private static void injected(String command, CallbackInfo ci, @Local(argsOnly = true) LocalRef<String> localRef) {
-        if (!ConfigOptions.TELEPORT_INTERCEPT.getAsBooleanFromValues(ConfigOptions.Values.ON_OFF)) return;
-        if (!RightClickMenu.useTpll()) return;
+    @Unique
+    private final static List<String> prefixes = List.of(
+            "execute in minecraft:overworld run tp",
+            "tp",
+            "minecraft:tp"
+    );
+
+    @ModifyVariable(method = "sendChatCommand", at = @At(value = "HEAD"), argsOnly = true)
+    private static String injected(String command) {
+        if (!ConfigOptions.TELEPORT_INTERCEPT.getAsBooleanFromValues(ConfigOptions.Values.ON_OFF)) return command;
+        if (!RightClickMenu.useTpll()) return command;
         //MinecraftClient.getInstance().player.sendMessage(Text.of("Command: " + command), false);
-        if (command.replaceFirst("execute in minecraft:overworld run tp", "").length() != command.length()) {
-            //MinecraftClient.getInstance().player.sendMessage(Text.of("Yay!"), false);
-            String[] arguments = command.replaceFirst("execute in minecraft:overworld run tp", "").split(" ");
-            //MinecraftClient.getInstance().player.sendMessage(Text.of(Arrays.toString(arguments)), false);
-            if (arguments.length != 5 && arguments.length != 7) return;
-            double[] latLon;
-            try {
-                latLon = Projection.to_geo(Double.parseDouble(arguments[2]), Double.parseDouble(arguments[4]));
-                //MinecraftClient.getInstance().player.sendMessage(Text.of("Converted to " + latLon[0] + ", "  + latLon[1]), false);
 
-                if (arguments.length == 5) command =
-                        "tpll " +
-                                latLon[0] + " " +
-                                latLon[1] + " " +
-                                arguments[3];
-                if (arguments.length == 7) command =
-                        "tpll " +
-                                latLon[0] + " " +
-                                latLon[1] + " " +
-                                arguments[3] + " " +
-                                arguments[5] + " " +
-                                arguments[6];
-            } catch (CoordinateValueError | NumberFormatException ignored) {}
-
+        String prefix = null;
+        for (String s : prefixes) {
+            if (command.startsWith(s)) {
+                prefix = s;
+                break;
+            }
         }
-        localRef.set(command);
+        if (prefix == null) return command;
+
+        //MinecraftClient.getInstance().player.sendMessage(Text.of("Yay!"), false);
+        String[] arguments = command.replaceFirst(prefix, "").split(" ");
+        //MinecraftClient.getInstance().player.sendMessage(Text.of(Arrays.toString(arguments)), false);
+        if (arguments.length != 5 && arguments.length != 7) return command;
+        double[] latLon;
+        try {
+            latLon = Projection.to_geo(Double.parseDouble(arguments[2]), Double.parseDouble(arguments[4]));
+            //MinecraftClient.getInstance().player.sendMessage(Text.of("Converted to " + latLon[0] + ", "  + latLon[1]), false);
+
+            if (arguments.length == 5) command =
+                    "tpll " +
+                            latLon[0] + " " +
+                            latLon[1] + " " +
+                            arguments[3];
+            if (arguments.length == 7) command =
+                    "tpll " +
+                            latLon[0] + " " +
+                            latLon[1] + " " +
+                            arguments[3] + " " +
+                            arguments[5] + " " +
+                            arguments[6];
+        } catch (CoordinateValueError | NumberFormatException ignored) {}
+
+        return command;
     }
 }
